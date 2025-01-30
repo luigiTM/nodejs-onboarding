e7523686d38ae23b05111195304f02547ffa3316
Luigi Tosin Misturini <luigitosin@gmail.com>
2025-01-30 11:49:32 -0300

    Removing insert from db migration

--- src/db/migrations/0001_create_currency_table.ts
+++ src/db/migrations/0001_create_currency_table.ts
@@ -2,18 +2,10 @@ import type { Knex } from "knex";
 import { Tables } from "../tables";
 
 export async function up(knex: Knex) {
-  return knex.schema
-    .createTable(Tables.currency, (table) => {
-      table.increments("id").primary().index();
-      table.text("acronym").notNullable();
-    })
-    .then(() => {
-      return knex(Tables.currency).insert([
-        { acronym: "UYU" },
-        { acronym: "USD" },
-        { acronym: "EUR" },
-      ]);
-    });
+  return knex.schema.createTable(Tables.currency, (table) => {
+    table.increments("id").primary().index();
+    table.text("acronym").notNullable();
+  });
 }
 
 export async function down(knex: Knex) {
