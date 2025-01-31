3416340458a750c2950a5be46bbd68f6e8f9ae8e
Luigi Tosin Misturini <luigitosin@gmail.com>
2025-01-31 14:22:36 -0300

    Added foreign keys indexes

--- src/db/migrations/0002_create_account_table.ts
+++ src/db/migrations/0002_create_account_table.ts
@@ -4,7 +4,7 @@ import { Tables } from "../tables";
 export async function up(knex: Knex) {
   return knex.schema.createTable(Tables.account, (table) => {
     table.uuid("id").primary().index();
-    table.uuid("user_id").references("id").inTable("user");
+    table.uuid("user_id").references("id").inTable("user").index();
     table.integer("currency_id").references("id").inTable("currency");
     table.specificType("balance", "money").defaultTo(0);
   });
--- src/db/migrations/0003_create_transaction_table.ts
+++ src/db/migrations/0003_create_transaction_table.ts
@@ -4,8 +4,12 @@ import { Tables } from "../tables";
 export async function up(knex: Knex) {
   return knex.schema.createTable(Tables.transaction, (table) => {
     table.uuid("id").primary().index();
-    table.uuid("source_account").references("id").inTable("account");
-    table.integer("destination_account").references("id").inTable("account");
+    table.uuid("source_account").references("id").inTable("account").index();
+    table
+      .integer("destination_account")
+      .references("id")
+      .inTable("account")
+      .index();
     table.specificType("amount", "money").notNullable().checkPositive();
     table.text("description").checkLength("<", 150);
     table.dateTime("created_at").notNullable().defaultTo(new Date());
