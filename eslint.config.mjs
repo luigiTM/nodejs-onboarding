3a13c67b32c4587c1b429b18ac6a6e24ebcd7cad
Luigi Tosin Misturini <luigitosin@gmail.com>
2025-02-17 11:18:15 -0300

    Fixing identation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,5 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+export default [{ files: ["**/*.{js,mjs,cjs,ts}"] }, { languageOptions: { globals: globals.browser } }, { ignores: ["dist/"] }, pluginJs.configs.recommended, ...tseslint.configs.recommended];
