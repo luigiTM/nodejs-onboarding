6390732243d9864b4ca43a5f0fb37beccc58f2e1
Luigi Tosin Misturini <luigitosin@gmail.com>
2025-02-21 16:06:14 -0300

    Some code refactoring

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -3,10 +3,7 @@ import User from "../../model/user";
 
 export interface AuthService {
   hashPassword(plainPassword: string): Promise<string>;
-  comparePassword(
-    plainPassword: string,
-    hashedPassword: string,
-  ): Promise<boolean>;
+  comparePassword(plainPassword: string, hashedPassword: string): Promise<boolean>;
   createToken(user: User): Promise<string>;
   verify(token: string): Promise<string | JwtPayload>;
 }
